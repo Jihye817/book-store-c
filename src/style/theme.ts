55860f4d0f6c14dc37d5e9f892d6579d4ff400fa
jihye <wlgul817@gmail.com>
2025-02-03 11:38:08 +0900

    feat: Input 컴포넌트 작성

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,12 +1,38 @@
 export type ThemeName = "light" | "dark";
-type ColorKey = "primary" | "secondary" | "third" | "background";
+export type ColorKey =
+  | "primary"
+  | "secondary"
+  | "third"
+  | "background"
+  | "border"
+  | "text";
+export type HeadingSize = "large" | "medium" | "small";
+export type ButtonSize = "large" | "medium" | "small";
+export type ButtonScheme = "primary" | "normal";
 
 interface Theme {
   name: ThemeName;
-  // color: {
-  //   [key in ColorKey]: string;
-  // };
   color: Record<ColorKey, string>;
+  heading: {
+    [key in HeadingSize]: {
+      fontSize: string;
+    };
+  };
+  button: {
+    [key in ButtonSize]: {
+      fontSize: string;
+      padding: string;
+    };
+  };
+  buttonScheme: {
+    [key in ButtonScheme]: {
+      color: string;
+      backgroundColor: string;
+    };
+  };
+  borderRadius: {
+    default: string;
+  };
 }
 
 export const light: Theme = {
@@ -16,16 +42,59 @@ export const light: Theme = {
     secondary: "blue",
     third: "green",
     background: "lightgrey",
+    border: "grey",
+    text: "black",
+  },
+  heading: {
+    large: {
+      fontSize: "2rem",
+    },
+    medium: {
+      fontSize: "1.5rem",
+    },
+    small: {
+      fontSize: "1rem",
+    },
+  },
+  button: {
+    large: {
+      fontSize: "1.5rem",
+      padding: "1rem 2rem",
+    },
+    medium: {
+      fontSize: "1rem",
+      padding: "0.5rem 1rem",
+    },
+    small: {
+      fontSize: "0.75rem",
+      padding: "0.25rem 0.5rem",
+    },
+  },
+  buttonScheme: {
+    primary: {
+      color: "white",
+      backgroundColor: "midnightblue",
+    },
+    normal: {
+      color: "black",
+      backgroundColor: "lightgrey",
+    },
+  },
+  borderRadius: {
+    default: "4px",
   },
 };
 
 export const dark: Theme = {
+  ...light,
   name: "dark",
   color: {
     primary: "coral",
     secondary: "darkblue",
     third: "darkgreen",
     background: "midnightblue",
+    border: "grey",
+    text: "black",
   },
 };
 
