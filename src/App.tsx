00a8f7367baef8ed90027000f7972afdfcfb5e9a
jihye <wlgul817@gmail.com>
2025-02-03 13:44:54 +0900

    feat: Header, Footer 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Home from "./pages/Home";
 import Layout from "./components/Layout/Layout";
-import ThemeSwitcher from "./components/header/ThemeSwitcher";
 import { BookStoreThemeProvider } from "./context/themeContext";
 
 function App() {
   return (
     <BookStoreThemeProvider>
-      <ThemeSwitcher />
       <Layout>
         <Home />
       </Layout>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,8 @@
 import React from "react";
-import Title from "../components/common/Title";
-import Button from "../components/common/Button";
-import InputText from "../components/common/InputText";
 
 function Home() {
   return (
     <>
-      <Title size="medium" color="background">
-        제목 테스트
-      </Title>
-      <Button size="large" scheme="primary">
-        버튼 테스트
-      </Button>
-      <InputText placeholder="여기에 입력하세요" />
       <div>home body</div>
     </>
   );
