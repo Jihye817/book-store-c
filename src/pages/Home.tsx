baca182957b74549eda72fe6f82dd647d377199a
jihye <wlgul817@gmail.com>
2025-02-10 14:19:49 +0900

    feat: 리팩토링 - 중복 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
 function Home() {
   return (
-    <>
-      <div>home body</div>
-    </>
+    <HomeStyle>
+      <h1>Home</h1>
+    </HomeStyle>
   );
 }
+
+const HomeStyle = styled.div``;
 
 export default Home;
