7b7c2f2dba0b0adbf4408379c03923daf547edcb
jihye <wlgul817@gmail.com>
2025-02-11 10:12:58 +0900

    feat: 모킹 서버 작성

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,22 @@ import "./index.css";
 import App from "./App";
 import { state, ThemeContext } from "./context/themeContext";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <ThemeContext.Provider value={state}>
-      <App />
-    </ThemeContext.Provider>
-  </React.StrictMode>
-);
+async function mountApp() {
+  if (process.env.NODE_ENV === "development") {
+    const { worker } = require("./mock/browser");
+    await worker.start(); //msw 시작
+  }
+
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <ThemeContext.Provider value={state}>
+        <App />
+      </ThemeContext.Provider>
+    </React.StrictMode>
+  );
+}
+
+mountApp();
